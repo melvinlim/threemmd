9d87ec2448198a8cab30ec08416a1a184c2b08d7
Melvin Lim <dxlim@outlook.com>
2024-04-14 12:07:28 -0700

    made miku rotate slowly, put checkerboard generation code in a function, and raised the camera angle up a little.

--- main.js
+++ main.js
@@ -10,18 +10,16 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-/*
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-*/
-camera.position.z = 5;
+
+//this is pointless if using orbitcontrols.
+//camera.up = new THREE.Vector3(0, 0, -1);
+//camera.lookAt(new THREE.Vector3(0, 10, 0));
+
+camera.position.y = 15;
 camera.position.z = 25;
-camera.position.y = 10;
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 5, 0);
+controls.target.set(0, 10, 0);		//camera looks at this point if using orbitcontrols.
 controls.update();
 
 class ColorGUIHelper {
@@ -37,25 +35,28 @@ class ColorGUIHelper {
   }
 }
 
-const planeSize = 40;
- 
-const texLoader = new THREE.TextureLoader();
-const texture = texLoader.load('checker.png');
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.magFilter = THREE.NearestFilter;
-texture.colorSpace = THREE.SRGBColorSpace;
-const repeats = planeSize / 2;
-texture.repeat.set(repeats, repeats);
+function createCheckerboard(scene, planeSize) {
 
-const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-const planeMat = new THREE.MeshPhongMaterial({
-  map: texture,
-  side: THREE.DoubleSide,
-});
-const mesh = new THREE.Mesh(planeGeo, planeMat);
-mesh.rotation.x = Math.PI * -.5;
-scene.add(mesh);
+	const texLoader = new THREE.TextureLoader();
+	const texture = texLoader.load('checker.png');
+	texture.wrapS = THREE.RepeatWrapping;
+	texture.wrapT = THREE.RepeatWrapping;
+	texture.magFilter = THREE.NearestFilter;
+	texture.colorSpace = THREE.SRGBColorSpace;
+	const repeats = planeSize / 2;
+	texture.repeat.set(repeats, repeats);
+
+	const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+	const planeMat = new THREE.MeshPhongMaterial({
+		map: texture,
+		side: THREE.DoubleSide,
+	});
+	const mesh = new THREE.Mesh(planeGeo, planeMat);
+	mesh.rotation.x = Math.PI * -.5;
+	scene.add(mesh);
+}
+const planeSize = 40;
+createCheckerboard(scene, planeSize);
 
 const color = 0xFFFFFF;
 const intensity = 1;
@@ -106,8 +107,8 @@ const miku = await loader.loadAsync(
 
 function animate() {
 	requestAnimationFrame( animate );
-	//cube.rotation.x += 0.01;
-	//cube.rotation.y += 0.01;
+	//miku.rotation.x += 0.01;
+	miku.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 animate();
