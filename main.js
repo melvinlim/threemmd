3d7888eae458b19ea9bc22bc35794f3c0e12536e
Melvin Lim <dxlim@outlook.com>
2024-04-15 00:15:00 -0700

    added loading manager to make sure everything is loaded before starting.

--- main.js
+++ main.js
@@ -62,8 +62,26 @@ const animationPath = 'mmdanimations/default2.vmd'
 
 const helper = new MMDAnimationHelper();
 
+const manager = new THREE.LoadingManager();
+manager.onStart = function (url, itemsLoaded, itemsTotal) {
+	console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+};
+
+manager.onLoad = function () {
+	console.log('Loading complete!');
+	//scene.add(mmdModel);
+};
+
+manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+	console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+};
+
+manager.onError = function (url) {
+	console.log('There was an error loading ' + url);
+};
+
 // Load MMD resources and add to helper
-new MMDLoader().loadWithAnimation(
+new MMDLoader(manager).loadWithAnimation(
 	modelPath,
 	animationPath,
 	function (mmd) {
