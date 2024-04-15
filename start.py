c28703b8fc131b1ae1c2e99da1df16a6c4705995
Melvin Lim <dxlim@outlook.com>
2024-04-14 17:56:29 -0700

    switched from using http.server module to script from stackoverflow in order to use utf-8 encoding and avoid browser inspector warnings.

---
+++ start.py
@@ -0,0 +1,9 @@
+#python3 -m http.server
+
+#from https://stackoverflow.com/questions/15288891/how-can-i-serve-files-with-utf-8-encoding-using-python-simplehttpserver
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from http.server import test, SimpleHTTPRequestHandler as RH
+RH.extensions_map={k:v+';charset=UTF-8' for k,v in RH.extensions_map.items()}
+test(RH)
