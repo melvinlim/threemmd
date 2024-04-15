70c903999973c5052f98224c75dffbe65bed472a
Melvin Lim <dxlim@outlook.com>
2024-04-14 18:52:26 -0700

    removed server startup script from repo.

--- start.py
+++
@@ -1,9 +0,0 @@
-#python3 -m http.server
-
-#from https://stackoverflow.com/questions/15288891/how-can-i-serve-files-with-utf-8-encoding-using-python-simplehttpserver
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
-from http.server import test, SimpleHTTPRequestHandler as RH
-RH.extensions_map={k:v+';charset=utf-8' for k,v in RH.extensions_map.items()}
-test(RH)
